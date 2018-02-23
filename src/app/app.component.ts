ba13aa66c02f133f9ded3be14e07b2bebc473720
Kieran Chadwick <kieran.chadwick@outlook.com>
2018-02-23 22:28:44 +0000

    fixed sector2 time bugs + sector event colours

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -123,13 +123,13 @@ export class AppComponent {
             entry.sectorEvent = null;
 
             if (entry.currentSectorTime2 !== -1) {
+              const sector2Time = entry.currentSectorTime2 - entry.currentSectorTime1;
 
               // sector 2 completed
-              const gap = isEmpty(this.overallBestLap) ? 0 : entry.currentSectorTime2 - this.overallBestLap.sector_2;
+              const gap = isEmpty(this.overallBestLap) ? 0 : sector2Time - this.overallBestLap.sector_2;
               sector2Event.gap = gap.toFixed(3);
 
               // SB, PB or slower?
-              const sector2Time = entry.currentSectorTime2 - entry.currentSectorTime1;
               if (isEmpty(this.overallBestLap) || sector2Time < this.overallBestLap.sector_2) {
                 sector2Event.state = 'SESSION_BEST';
               } else if (isEmpty(driverLap.best_lap) || sector2Time < driverLap.best_lap.sector_2) {
@@ -184,5 +184,11 @@ export class AppComponent {
       },
       error => console.log(error)
     );
+  }
+  
+  getSectorColour(state: String): String {
+    if (state === 'SESSION_BEST') return 'purple';
+    if (state === 'PERSONAL_BEST') return 'green';
+    if (state === 'DOWN') return 'red';
   }
 }

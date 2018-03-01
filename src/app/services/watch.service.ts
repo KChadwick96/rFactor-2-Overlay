7c105e06a81531262df46e009ec27c1aac61f77e
Kieran Chadwick <kieran.chadwick@outlook.com>
2018-03-01 21:26:06 +0000

    big code refactor to move code to watch.service

--- src/app/services/watch.service.ts
+++ src/app/services/watch.service.ts
@@ -1,57 +1,226 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { sortBy, isEmpty } from 'lodash';
 import { Observable } from 'rxjs/Observable';
 import { Observer } from 'rxjs/Observer';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
+import { ConfigService } from './config.service';
 import { environment } from '../../environments/environment';
 
 @Injectable()
 export class WatchService {
 
-  private baseUrl: string = 'http://localhost:5397/rest/watch';
+  private DATA_REFRESH_RATE = 2000;
+  private HOLD_LAP_INFO_DELAY = 10000;
+
+  private _baseUrl: string = 'http://localhost:5397/rest/watch';
+  private _sessionData: any;
+  private _driverLaps: any;   // hold persistent driverlap info
+  private _focusedDriver: any;
+  private _overallBestLap: any;
+  private _teamsConfig: any;
 
   constructor(
-    private http: Http
+    private http: Http,
+    private config: ConfigService
   ) {}
 
-  getSession(): Observable<any> {
+  session(): Observable<any> {
+    
+    if (this._driverLaps === undefined) this._driverLaps = {};
+    if (this._focusedDriver === undefined) this._focusedDriver = null;
+    if (this._overallBestLap === undefined) this._overallBestLap = null;
+    this._teamsConfig = this.config.getTeamsConfig();
 
-    // dev mode
-    if (!environment.production) {
-      return this._getFakeSession();
+    // fetch session data
+    if (environment.production) {
+      setInterval(() => {
+        this._sessionObservable().subscribe(session => this._sessionData = session);
+      });
     }
 
+    return Observable.create(observer => {
+      setInterval(() => {
+
+        // test data
+        if (!environment.production) {
+          const processedStandings = this._processStandings(sampleStandingsData);
+          return observer.next({
+            session_info: sampleSessionData,
+            standings: processedStandings,
+            focused_driver: this._focusedDriver,
+            overall_best_lap: this._overallBestLap
+          });
+        }
+
+        // empty or invalid session, return null data 
+        if (this._sessionData === undefined || this._sessionData.session === 'INVALID') {
+          return observer.next({
+            session_info: null,
+            standings: [],
+            focused_driver: this._focusedDriver,
+            overall_best_lap: null
+          });
+        }
+
+        // live data
+        this._standingsObservable().subscribe(standings => {
+          const processedStandings = this._processStandings(standings);
+          observer.next({
+            session_info: this._sessionData,
+            standings: processedStandings,
+            focused_driver: this._focusedDriver,
+            overall_best_lap: this._overallBestLap
+          });
+        });
+
+      }, this.DATA_REFRESH_RATE);
+    });
+  }
+
+  _processStandings(standings: Array<any>): Array<any> {
+    let processed = [];
+
+    processed = sortBy(standings, 'position');
+
+    processed.forEach(entry => {
+      const driverName = entry.driverName;
+
+      // does the driver exist in the driverlaps object
+      if (this._driverLaps[driverName] === undefined) {
+        this._initialiseDriverLap(driverName);
+      }
+
+      const driverLap = this._driverLaps[driverName];
+      entry.gapEvent = null;
+      entry.gapToLeader = this._gapToBest(entry.bestLapTime);
+
+      // has the driver just finished a lap
+      const lapsCheckedDifference = entry.lapsCompleted - driverLap.laps_checked;
+      if (lapsCheckedDifference === 1 && entry.lastLapTime > -1) {
+
+        const lap = {
+          sector_1: entry.lastSectorTime1,
+          sector_2: entry.lastSectorTime2 - entry.lastSectorTime1,
+          sector_3: entry.lastLapTime - entry.lastSectorTime2,
+          total: entry.lastLapTime
+        }
+
+        // gap state + and assign to entry
+        const personalBest = (isEmpty(driverLap.best_lap)) ? null : driverLap.best_lap.total;
+        const state = this._getLapState('sector_2', lap.total, personalBest);
+        const gap = this._gapToBest(entry.lastLapTime);
+        entry.gapEvent = {state, gap};
+
+        // is this their fastest?
+        if (driverLap.best_lap === null || driverLap.total > lap.total) {
+          driverLap.best_lap = lap;
+        }
+
+        // keep the last lap info on screen
+        driverLap.last_lap_hold = {counter: 0, lap, gap};
+
+        // is this the overall fastest?
+        if (isEmpty(this._overallBestLap.total) || this._overallBestLap.total > lap.total) {
+          this._overallBestLap = lap;
+        }
+      }
+      driverLap.laps_checked = entry.lapsCompleted;
+
+      // are we still showing last lap info
+      if (driverLap.last_lap_hold) {
+        if (driverLap.last_lap_hold.counter > this.HOLD_LAP_INFO_DELAY) {
+          driverLap.last_lap_hold = null;
+        } else {
+          driverLap.last_lap_hold.counter += this.DATA_REFRESH_RATE;
+        }
+      }
+      entry.lastLapHold = driverLap.lastLapHold;
+
+      // have they just completed the 1st or 2nd sector
+      if (entry.currentSectorTime1 !== -1) {
+
+        if (entry.currentSectorTime2 !== -1) {
+
+          // sector 2 completed
+          let gapValue = 0;
+          if (!isEmpty(this._overallBestLap)) {
+            gapValue = entry.currentSectorTime2 - (this._overallBestLap.sector_1 + this._overallBestLap.sector_2);
+          }
+          const gap = (gapValue > 0 ? '+' : '') + gapValue.toFixed(3);
+
+          // gap state + and assign to entry
+          const personalBest = (isEmpty(driverLap.best_lap)) ? null : driverLap.best_lap.sector_1 + driverLap.best_lap.sector_2;
+          const state = this._getLapState('sector_2', entry.currentSector2Time, personalBest);
+          entry.gapEvent = {state, gap};
+        } else {
+
+          // sector 1 completed
+          const gapValue = isEmpty(this._overallBestLap) ? 0 : entry.currentSectorTime1 - this._overallBestLap.sector_1;
+          const gap = (gapValue > 0 ? '+' : '') + gapValue.toFixed(3);
+
+          // gap state + and assign to entry
+          const personalBest = (isEmpty(driverLap.best_lap)) ? null : driverLap.best_lap.sector_1;
+          const state = this._getLapState('sector_1', entry.currentSectorTime1, personalBest);
+          entry.gapEvent = {state, gap};
+        }
+      }
+
+      entry.colour = this._getTeamColour(entry.carClass.toLowerCase());
+      if (entry.focus) this._focusedDriver = entry; 
+    });
+
+    return processed;
+  }
+
+  _initialiseDriverLap(driverName: string): void {
+    this._driverLaps[driverName] = {
+      best_lap: null,
+      laps_checked: 0
+    }
+  }
+
+  _gapToBest(lapTime: number): string {
+    let gapValue = 0;
+    if (!isEmpty(this._overallBestLap)) {
+      gapValue = lapTime - this._overallBestLap.total;
+    }
+    return (gapValue > 0 ? '+' : '') + gapValue.toFixed(3);
+  }
+
+  _getLapState(sector: string, current: number, driverBest: number): string {
+    if (isEmpty(this._overallBestLap) || current < this._overallBestLap[sector]) {
+      return 'SESSION_BEST';
+    } else if (driverBest === null || current < driverBest) {
+      return 'PERSONAL_BEST';
+    } else {
+      return 'DOWN';
+    }
+  }
+
+  _getTeamColour(carClass: string): string {
+    let colour = '#FFF';
+    if (this._teamsConfig[carClass]) {
+      colour = this._teamsConfig[carClass].colour;
+    }
+
+    return colour;
+  }
+
+  _standingsObservable(): Observable<any> {
     return this.http
-      .get(`${this.baseUrl}/sessionInfo`)
+      .get(`${this._baseUrl}/standings`)
       .map(this._mapResponse)
       .catch(this._handleError);
   }
 
-  getStandings(): Observable<any> {
-
-    // dev mode
-    if (!environment.production) {
-      return this._getFakeStandings();
-    }
-
+  _sessionObservable(): Observable<any> {
     return this.http
-      .get(`${this.baseUrl}/standings`)
+      .get(`${this._baseUrl}/sessionInfo`)
       .map(this._mapResponse)
       .catch(this._handleError);
-  }
-
-  _getFakeSession(): Observable<any> {
-    return Observable.create((observer: Observer<any>) => {
-      observer.next(sampleSessionData);
-    });
-  }
-
-  _getFakeStandings(): Observable<any> {
-    return Observable.create((observer: Observer<any>) => {
-      observer.next(sampleStandingsData);
-    });
   }
 
   _mapResponse(response: Response): any {
@@ -108,4 +277,3 @@ const sampleStandingsData = [
   {"position":4,"driverName":"AOR Test Driver - Green","bestLapTime":106.20903015136719,"pitstops":0,"pitting":false,"lastLapTime":106.20903015136719,"vehicleName":"AOR Test Driver - Green","timeBehindNext":-60.82139587402344,"timeBehindLeader":-48.648197174072266,"lapsBehindLeader":0,"lapsBehindNext":0,"currentSectorTime1":34.31390380859375,"currentSectorTime2":79.15994262695312,"lastSectorTime1":36.229339599609375,"lastSectorTime2":81.39878845214844,"focus":false,"carClass":"AOR Test Team","slotID":8,"carStatus":"NONE","lapsCompleted":2,"hasFocus":false},{"position":7,"driverName":"Scott Davison","bestLapTime":-1.0,"pitstops":0,"pitting":false,"lastLapTime":0.0,"vehicleName":"Scott Davison #13","timeBehindNext":-74.79524230957031,"timeBehindLeader":0.0,"lapsBehindLeader":1,"lapsBehindNext":0,"currentSectorTime1":-1.0,"currentSectorTime2":-1.0,"lastSectorTime1":0.0,"lastSectorTime2":0.0,"focus":false,"carClass":"Defiance Racing","slotID":9,"carStatus":"NONE","lapsCompleted":0,"hasFocus":false},
   {"position":11,"driverName":"Craig Baxter","bestLapTime":-1.0,"pitstops":0,"pitting":false,"lastLapTime":-1.0,"vehicleName":"Craig Baxter #9","timeBehindNext":-55.17249298095703,"timeBehindLeader":43.16745376586914,"lapsBehindLeader":0,"lapsBehindNext":0,"currentSectorTime1":33.73773193359375,"currentSectorTime2":78.95513916015625,"lastSectorTime1":-1.0,"lastSectorTime2":-1.0,"focus":false,"carClass":"Defiance Racing","slotID":10,"carStatus":"NONE","lapsCompleted":1,"hasFocus":false}
 ]
-

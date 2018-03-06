db9857eaa0f8e20c7ffa4a8e71940adae642f97b
Kieran Chadwick <kieran.chadwick@outlook.com>
2018-03-06 22:35:04 +0000

    started adding flags for drivers

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -5,38 +5,92 @@ export class ConfigService {
 
     constructor() {}
 
-    getTeamsConfig(): Object {
-        return fakeTeamConfig;
+    getConfig(): any {
+        return config;
     }
 }
 
-
-const fakeTeamConfig = {
-    "krt": {
-        "colour": "#e01b25"
+const config = {
+    teams: {
+        "krt": {
+            "colour": "#e01b25"
+        },
+        "sabre racing": {
+            "colour": "#07b5f1"
+        },
+        "defiance racing": {
+            "colour": "#a000d2"
+        },
+        "disruptive tech racing": {
+            "colour": "#3c3c3d"
+        },
+        "acr zakspeed": {
+            "colour": "#e8e8e8"
+        },
+        "zesty racing": {
+            "colour": "#e28c28"
+        },
+        "idos motorsport": {
+            "colour": "#000000"
+        },
+        "vi race team": {
+            "colour": "#e5e5e5"
+        },
+        "s2 racing": {
+            "colour": "#002b91"
+        } 
     },
-    "sabre racing": {
-        "colour": "#07b5f1"
-    },
-    "defiance racing": {
-        "colour": "#a000d2"
-    },
-    "disruptive tech racing": {
-        "colour": "#3c3c3d"
-    },
-    "acr zakspeed": {
-        "colour": "#e8e8e8"
-    },
-    "zesty racing": {
-        "colour": "#e28c28"
-    },
-    "idos motorsport": {
-        "colour": "#000000"
-    },
-    "vi race team": {
-        "colour": "#e5e5e5"
-    },
-    "s2 racing": {
-        "colour": "#002b91"
+    drivers: {
+        "kieran chadwick": {
+            "flag": "https://imgur.com/4yAOniI.png"
+        },
+        "craig baxter": {
+            "flag": "https://imgur.com/0apJFaG.png"
+        },
+        "scott davison": {
+            "flag": "https://imgur.com/4yAOniI.png"
+        },
+        "evan rice": {
+            "flag": "https://imgur.com/lqEcsyy.png"
+        },
+        "sam carpenter": {
+            "flag": "https://imgur.com/4yAOniI.png"
+        },
+        "cian white": {
+            "flag": "https://imgur.com/Wno1zTe.png"
+        },
+        "mac shepherd": {
+            "flag": "https://imgur.com/4yAOniI.png"
+        },
+        "kevin cox": {
+            "flag": "https://imgur.com/4yAOniI.png"
+        },
+        "ross woodford": {
+            "flag": "https://imgur.com/4yAOniI.png"
+        },
+        "tino naukkarinen": {
+            "flag": "https://imgur.com/VIJffgO.png"
+        }
     }
 }
+
+// barbados  https://imgur.com/wfwSvVC.png
+// canada https://imgur.com/wfwSvVC.png
+// estonia https://imgur.com/lQY7jwt
+// usa https://imgur.com/lqEcsyy
+// finland https://imgur.com/VIJffgO
+// france https://imgur.com/o3kblwK
+// germany https://imgur.com/uXTuiD0
+// czech https://imgur.com/NW1W2Xq
+// ireland https://imgur.com/Wno1zTe
+// italy https://imgur.com/isyV4ka
+// lithuania https://imgur.com/MUamoPt
+// greece https://imgur.com/YsHTDTe
+// south africa https://imgur.com/6q7RieM
+// spain https://imgur.com/kWjTroJ
+// netherlands https://imgur.com/fhXmGJQ
+// portugal https://imgur.com/i7q5I4M
+// sweden https://imgur.com/pnoBMKg
+// uk https://imgur.com/4yAOniI
+// uruguay https://imgur.com/uySRbrm
+// scotland https://imgur.com/0apJFaG
--- src/app/services/watch.service.ts
+++ src/app/services/watch.service.ts
@@ -21,6 +21,7 @@ export class WatchService {
   private _focusedDriver: any;
   private _overallBestLap: any;
   private _teamsConfig: any;
+  private _driversConfig: any;
 
   constructor(
     private http: Http,
@@ -32,7 +33,10 @@ export class WatchService {
     if (this._driverLaps === undefined) this._driverLaps = {};
     if (this._focusedDriver === undefined) this._focusedDriver = null;
     if (this._overallBestLap === undefined) this._overallBestLap = null;
-    this._teamsConfig = this.config.getTeamsConfig();
+
+    const config = this.config.getConfig();
+    this._teamsConfig = config.teams;
+    this._driversConfig = config.drivers;
 
     // fetch session data
     if (environment.production) {
@@ -165,6 +169,7 @@ export class WatchService {
       }
 
       entry.colour = this._getTeamColour(entry.carClass.toLowerCase());
+      entry.flag = this._getDriverFlag(entry.driverName);
       if (entry.focus) this._focusedDriver = entry;
     });
 
@@ -256,6 +261,15 @@ export class WatchService {
     return colour;
   }
 
+  _getDriverFlag(driverName: string): string {
+    driverName = driverName.toLowerCase();
+    if (this._driversConfig[driverName]) {
+      return this._driversConfig[driverName].flag
+    }
+
+    return null;
+  }
+
   _standingsObservable(): Observable<any> {
     return this.http
       .get(`${this._baseUrl}/standings`)
@@ -316,11 +330,12 @@ const sampleStandingsData = [
     "lapsCompleted":0,
     "hasFocus":false
   },
-  {"position":5,"driverName":"Mattia Silva","bestLapTime":-1.0,"pitstops":0,"pitting":false,"lastLapTime":-1.0,"vehicleName":"Mattia Silva #47","timeBehindNext":0.0,"timeBehindLeader":0.0,"lapsBehindLeader":1,"lapsBehindNext":1,"currentSectorTime1":-1.0,"currentSectorTime2":-1.0,"lastSectorTime1":-1.0,"lastSectorTime2":-1.0,"focus":true,"carClass":"Disruptive Tech Racing","slotID":1,"carStatus":"FINISHED","lapsCompleted":1,"hasFocus":true},
+  {"position":5,"driverName":"Mattia Silva","bestLapTime":-1.0,"pitstops":0,"pitting":false,"lastLapTime":-1.0,"vehicleName":"Mattia Silva #47","timeBehindNext":0.0,"timeBehindLeader":0.0,"lapsBehindLeader":1,"lapsBehindNext":1,"currentSectorTime1":-1.0,"currentSectorTime2":-1.0,"lastSectorTime1":-1.0,"lastSectorTime2":-1.0,"focus":false,"carClass":"Disruptive Tech Racing","slotID":1,"carStatus":"FINISHED","lapsCompleted":1,"hasFocus":false},
   {"position":6,"driverName":"AOR Test Driver - Red","bestLapTime":-1.0,"pitstops":0,"pitting":true,"lastLapTime":0.0,"vehicleName":"AOR Test Driver - Red","timeBehindNext":0.0,"timeBehindLeader":0.0,"lapsBehindLeader":2,"lapsBehindNext":1,"currentSectorTime1":-1.0,"currentSectorTime2":-1.0,"lastSectorTime1":0.0,"lastSectorTime2":0.0,"focus":false,"carClass":"AOR Test Team","slotID":2,"carStatus":"PITTING","lapsCompleted":0,"hasFocus":false},
   {"position":10,"driverName":"AOR Test Driver - Silver","bestLapTime":-1.0,"pitstops":0,"pitting":false,"lastLapTime":-1.0,"vehicleName":"AOR Test Driver - Silver","timeBehindNext":0.0,"timeBehindLeader":0.0,"lapsBehindLeader":1,"lapsBehindNext":0,"currentSectorTime1":33.345458984375,"currentSectorTime2":-1.0,"lastSectorTime1":-1.0,"lastSectorTime2":-1.0,"focus":false,"carClass":"AOR Test Team","slotID":3,"carStatus":"DNF","lapsCompleted":1,"hasFocus":false},
   {"position":3,"driverName":"AOR Test Driver - Yellow","bestLapTime":104.36524963378906,"pitstops":0,"pitting":false,"lastLapTime":104.36524963378906,"vehicleName":"AOR Test Driver - Yellow","timeBehindNext":69.04061126708984,"timeBehindLeader":12.173198699951172,"lapsBehindLeader":0,"lapsBehindNext":0,"currentSectorTime1":-1.0,"currentSectorTime2":-1.0,"lastSectorTime1":33.46806335449219,"lastSectorTime2":79.11146545410156,"focus":false,"carClass":"AOR Test Team","slotID":4,"carStatus":"FINISHED","lapsCompleted":2,"hasFocus":false},
-  {"position":1,"driverName":"Sam Carpenter","bestLapTime":102.99269104003906,"pitstops":0,"pitting":false,"lastLapTime":102.99269104003906,"vehicleName":"Sam Carpenter #46","timeBehindNext":0.0,"timeBehindLeader":0.0,"lapsBehindLeader":0,"lapsBehindNext":0,"currentSectorTime1":33.90283203125,"currentSectorTime2":-1.0,"lastSectorTime1":33.18882751464844,"lastSectorTime2":77.81517028808594,"focus":false,"carClass":"Sabre Racing","slotID":5,"carStatus":"DNF","lapsCompleted":2,"hasFocus":false},{"position":8,"driverName":"Tino Naukkarinen","bestLapTime":-1.0,"pitstops":0,"pitting":false,"lastLapTime":-1.0,"vehicleName":"Tino Naukkarinen #10","timeBehindNext":0.0,"timeBehindLeader":67.22758483886719,"lapsBehindLeader":0,"lapsBehindNext":0,"currentSectorTime1":32.540618896484375,"currentSectorTime2":-1.0,"lastSectorTime1":-1.0,"lastSectorTime2":-1.0,"focus":false,"carClass":"ACR Zakspeed","slotID":6,"carStatus":"DNF","lapsCompleted":1,"hasFocus":false},{"position":2,"driverName":"AOR Test Driver - Black","bestLapTime":103.86192321777344,"pitstops":0,"pitting":false,"lastLapTime":105.26962280273438,"vehicleName":"AOR Test Driver - Black","timeBehindNext":0.0,"timeBehindLeader":0.0,"lapsBehindLeader":0,"lapsBehindNext":0,"currentSectorTime1":-1.0,"currentSectorTime2":-1.0,"lastSectorTime1":34.29595947265625,"lastSectorTime2":79.72906494140625,"focus":false,"carClass":"AOR Test Team","slotID":7,"carStatus":"FINISHED","lapsCompleted":3,"hasFocus":false},
+  {"position":1,"driverName":"Sam Carpenter","bestLapTime":102.99269104003906,"pitstops":0,"pitting":false,"lastLapTime":102.99269104003906,"vehicleName":"Sam Carpenter #46","timeBehindNext":0.0,"timeBehindLeader":0.0,"lapsBehindLeader":0,"lapsBehindNext":0,"currentSectorTime1":33.90283203125,"currentSectorTime2":-1.0,"lastSectorTime1":33.18882751464844,"lastSectorTime2":77.81517028808594,"focus":false,"carClass":"Sabre Racing","slotID":5,"carStatus":"DNF","lapsCompleted":2,"hasFocus":false},
+  {"position":8,"driverName":"Tino Naukkarinen","bestLapTime":-1.0,"pitstops":0,"pitting":false,"lastLapTime":-1.0,"vehicleName":"Tino Naukkarinen #10","timeBehindNext":0.0,"timeBehindLeader":67.22758483886719,"lapsBehindLeader":0,"lapsBehindNext":0,"currentSectorTime1":32.540618896484375,"currentSectorTime2":-1.0,"lastSectorTime1":-1.0,"lastSectorTime2":-1.0,"focus":true,"carClass":"ACR Zakspeed","slotID":6,"carStatus":"DNF","lapsCompleted":1,"hasFocus":false},{"position":2,"driverName":"AOR Test Driver - Black","bestLapTime":103.86192321777344,"pitstops":0,"pitting":false,"lastLapTime":105.26962280273438,"vehicleName":"AOR Test Driver - Black","timeBehindNext":0.0,"timeBehindLeader":0.0,"lapsBehindLeader":0,"lapsBehindNext":0,"currentSectorTime1":-1.0,"currentSectorTime2":-1.0,"lastSectorTime1":34.29595947265625,"lastSectorTime2":79.72906494140625,"focus":false,"carClass":"AOR Test Team","slotID":7,"carStatus":"FINISHED","lapsCompleted":3,"hasFocus":false},
   {"position":4,"driverName":"AOR Test Driver - Green","bestLapTime":106.20903015136719,"pitstops":0,"pitting":false,"lastLapTime":106.20903015136719,"vehicleName":"AOR Test Driver - Green","timeBehindNext":-60.82139587402344,"timeBehindLeader":-48.648197174072266,"lapsBehindLeader":0,"lapsBehindNext":0,"currentSectorTime1":34.31390380859375,"currentSectorTime2":79.15994262695312,"lastSectorTime1":36.229339599609375,"lastSectorTime2":81.39878845214844,"focus":false,"carClass":"AOR Test Team","slotID":8,"carStatus":"NONE","lapsCompleted":2,"hasFocus":false},{"position":7,"driverName":"Scott Davison","bestLapTime":-1.0,"pitstops":0,"pitting":false,"lastLapTime":0.0,"vehicleName":"Scott Davison #13","timeBehindNext":-74.79524230957031,"timeBehindLeader":0.0,"lapsBehindLeader":1,"lapsBehindNext":0,"currentSectorTime1":-1.0,"currentSectorTime2":-1.0,"lastSectorTime1":0.0,"lastSectorTime2":0.0,"focus":false,"carClass":"Defiance Racing","slotID":9,"carStatus":"NONE","lapsCompleted":0,"hasFocus":false},
   {"position":11,"driverName":"Craig Baxter","bestLapTime":-1.0,"pitstops":0,"pitting":false,"lastLapTime":-1.0,"vehicleName":"Craig Baxter #9","timeBehindNext":-55.17249298095703,"timeBehindLeader":43.16745376586914,"lapsBehindLeader":0,"lapsBehindNext":0,"currentSectorTime1":33.73773193359375,"currentSectorTime2":78.95513916015625,"lastSectorTime1":-1.0,"lastSectorTime2":-1.0,"focus":false,"carClass":"Defiance Racing","slotID":10,"carStatus":"NONE","lapsCompleted":1,"hasFocus":false}
 ]

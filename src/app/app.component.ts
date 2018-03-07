07a92993ee89ee6eb45b6b8a2137db658b00ad9d
Kieran Chadwick <kieran.chadwick@outlook.com>
2018-03-07 22:35:08 +0000

    tower mode for gap to leader + custom cycle schedules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 
-
 import { WatchService } from './services/watch.service';
 
 @Component({
@@ -13,7 +12,6 @@ import { WatchService } from './services/watch.service';
 export class AppComponent {
 
   data: any;
-  towerMode: string = 'BASIC'; // FASTEST_LAP_GAP, BASIC, GAP_TO_LEADER
 
   constructor(
     private watchService: WatchService,
@@ -22,7 +20,6 @@ export class AppComponent {
 
   ngOnInit(): void {
     this._startDataCycle();
-    this._startTowerAlternate();
   }
 
   _startDataCycle(): void {
@@ -30,20 +27,6 @@ export class AppComponent {
       this.data = data;
       this.cdRef.markForCheck();
     });
-  }
-
-  _startTowerAlternate(): void {
-    setInterval(() => {
-      if (this.towerMode === 'BASIC') {
-        this.towerMode = 'FASTEST_LAP_GAP';
-      } else {
-        this.towerMode = 'BASIC';
-      }
-    }, 10000);
-  }
-
-  _getTowerMode(): string {
-    return 'FASTEST_LAP_GAP';
   }
   
   _getSectorColour(state: string): string {
--- src/app/components/tower/tower.component.ts
+++ src/app/components/tower/tower.component.ts
@@ -6,12 +6,80 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./tower.component.scss']
 })
 export class TowerComponent {
+    private _raceSession: string;
+    private _interval;
+    private _mode;
+    private _schedules: any = {
+        quali: [
+            {
+                mode: 'BASIC' ,
+                length: 5   
+            },
+            {
+                mode: 'FASTEST_LAP_GAP',
+                length: 5
+            },
+            {
+                mode: 'GAP_TO_LEADER',
+                length: 10
+            }
+        ],
+        race: [
+            {
+                mode: 'BASIC' ,
+                length: 10   
+            },
+            {
+                mode: 'GAP_TO_LEADER',
+                length: 20
+            }
+        ]
+    };
+
     @Input() standings: any[] = [];
-    @Input() mode: string;
+
+    @Input() 
+    set raceSession(session: string) {
+        if (session !== this._raceSession) {
+            this._raceSession = session;
+            this._startCycle();
+        }
+    }
 
     constructor() {}
 
     ngOnInit(): void {
-        console.log(this.mode);
+        this._startCycle();
+    }
+
+    _startCycle(): void {
+        if (!this._raceSession) return;
+
+        // clear existing loop and select schedule based on race session
+        if (this._interval !== undefined) clearInterval(this._interval);
+        let schedule = this._schedules.quali;
+        if (this._raceSession.includes('RACE')) {
+            schedule = this._schedules.race;   
+        }
+
+        // go through the schedule and switch when we've reached the part.length
+        let currentScheduleIndex = 0;
+        let currentDuration = 0;
+        this._interval = setInterval(() => {
+            const part = schedule[currentScheduleIndex];
+            this._mode = part.mode;
+
+            if (currentDuration >= part.length * 1000) {
+                currentDuration = 0;
+                
+                if (schedule[currentScheduleIndex + 1]) {
+                    currentScheduleIndex++;
+                } else {
+                    currentScheduleIndex = 0;
+                }
+            } else {
+                currentDuration += 1000;
+            }
+        }, 1000);
     }
 }

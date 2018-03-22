b7b2b97b2952e777c6ff1795d460fe0a85afbf35
Kieran Chadwick <kieran.chadwick@outlook.com>
2018-03-22 20:33:37 +0000

    fixed session timer bug

--- src/app/pipes/to-minutes.pipe.ts
+++ src/app/pipes/to-minutes.pipe.ts
@@ -5,9 +5,11 @@ export class ToMinutesPipe implements PipeTransform {
     transform(seconds: number, showMilliseconds: boolean = false, showStartZero = true, alwaysShowMinutes = false): string {
         if (seconds == -1) return '-';
 
+        if (!showMilliseconds) seconds = Math.floor(seconds);
+
         const minutes = Math.floor(seconds / 60);
         seconds = seconds - minutes * 60;
-        let secondsStr = showMilliseconds ? seconds.toFixed(3) : Math.round(seconds);
+        let secondsStr = showMilliseconds ? seconds.toFixed(3) : seconds;
 
         secondsStr = (seconds <= 9 && showStartZero) ? `0${secondsStr}` : `${secondsStr}`;
         if (secondsStr === '60') secondsStr = '00';
